ae9860fd1d6d33e720bd0555dea4da41685c9283
RMHONGITHUB <82383829+RMHONGITHUB@users.noreply.github.com>
2021-04-13 09:59:25 +0500

    Update My First plugin.php

--- My First plugin.php
+++ My First plugin.php
@@ -5,7 +5,12 @@
 * Author: Mubashar Hussain
 * Author Uri:https://gionline.xyz/crypto/about
 * Version:1.0.0
-* Description: This is for just testing purpose of new plugins by using php
+* Description: This is for just testing purpose of plugins by using php.
 **/
 
 defined('ABSPATH') || die ("You can't access this file directly");
+
+register_activation_hook(__FILE__,"First_plg_activation")
+register_deactivation_hook(__FILE__,"First_plg_deactivation")
+register_uninstall_hook(__FILE__,"First_plg_uninstall")  
+
