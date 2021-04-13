4e1d32d72fa867dc843ee3c61ffe2ade33cb5c22
RMHONGITHUB <82383829+RMHONGITHUB@users.noreply.github.com>
2021-04-13 09:52:51 +0500

    Create My First plugin.php

---
+++ My First plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: My first plugin
+* Plugin Uri: https://gionline.xyz/crypto/home
+* Author: Mubashar Hussain
+* Author Uri:https://gionline.xyz/crypto/about
+* Version:1.0.0
+* Description: This is for just testing purpose of new plugins by using php
+**/
+
+defined('ABSPATH') || die ("You can't access this file directly");
